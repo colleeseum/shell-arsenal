4cc4e1e002f25329f785f156b26495d8b793eb5d
Serge Colle <serge.colle@stardog.com>
2026-05-05 12:53:07 -0400

    codex-pr-review and some jq alias

--- aliases/kubectl.sh
+++ aliases/kubectl.sh
@@ -6,3 +6,25 @@ alias kdcr='kubectl describe certificaterequest'
 alias kgo='kubectl get order'
 alias kdo='kubectl describe order'
 alias kge='kubectl get events --watch'
+
+jcat() {
+  jq -R 'fromjson' "$@"
+}
+
+kljq() {
+  kl "$@" | jq -R 'fromjson'
+}
+
+klfjq() {
+  kl -f "$@" | jq -R 'fromjson'
+}
+
+if [[ -n ${ZSH_VERSION-} ]] && (( $+functions[compdef] )); then
+  _shell_arsenal_kubectl_pods() {
+    local -a pods
+    pods=("${(@f)$(kubectl get pods --no-headers -o custom-columns=NAME:.metadata.name 2>/dev/null)}")
+    _describe -t pods 'pods' pods
+  }
+
+  compdef _shell_arsenal_kubectl_pods kljq klfjq
+fi
